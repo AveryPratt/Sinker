aa7082b0e6980caee579b36219f01eeb389dc210
Avery Pratt <apratt91@gmail.com>
2019-08-02 22:54:13 -0700

    got light to turn off inside basket

---
+++ Sinker/Assets/BallLight.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallLight : MonoBehaviour
+{
+    public Light Light;
+
+    private float TargetIntensity;
+
+    private void Start()
+    {
+        TargetIntensity = Light.intensity;
+    }
+
+    public void Extinguish()
+    {
+        TargetIntensity = 0;
+    }
+
+    private void Update()
+    {
+        Light.intensity = Mathf.Lerp(Light.intensity, TargetIntensity, Time.deltaTime);
+    }
+}
