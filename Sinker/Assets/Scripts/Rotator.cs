1a1e6dbc1762b21956fdcc41b9fd9b07b7b2b9fc
Avery Pratt <apratt91@gmail.com>
2019-08-04 02:14:27 -0700

    added bigger 5th level

---
+++ Sinker/Assets/Scripts/Rotator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    public float SpeedMultiplier = 1;
+
+    private void Update()
+    {
+        transform.Rotate(0, 0, Time.deltaTime * 90 * SpeedMultiplier);
+    }
+}
